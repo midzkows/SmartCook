dfc5e52baa709ef1a8161281d02b4359bc6786b6
margaret <idzkowska0malgorzata@gmail.com>
2018-10-06 18:32:59 +0200

    rrStart working.
    Menu is done.

--- app/src/main/java/com/example/dev/smartcook/MainActivity.java
+++ app/src/main/java/com/example/dev/smartcook/MainActivity.java
@@ -1,52 +1,27 @@
 package com.example.dev.smartcook;
 
-import android.support.annotation.NonNull;
-import android.support.design.widget.NavigationView;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
-    private DrawerLayout myDrawerLayout;
-    private ActionBarDrawerToggle myToggle;
+public class MainActivity extends Activity {
+
+    public Button start_heating;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        myDrawerLayout=(DrawerLayout) findViewById(R.id.main);
-        myToggle=new ActionBarDrawerToggle(this, myDrawerLayout, R.string.open, R.string.close);
-        myDrawerLayout.addDrawerListener(myToggle);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        getSupportActionBar().setHomeButtonEnabled(true);
-        myToggle.syncState();
+        start_heating = (Button) findViewById(R.id.start_heating);
+        start_heating.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent heating = new Intent(MainActivity.this, examine.class);
+                startActivity(heating);
+            }
+        });
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        if (myToggle.onOptionsItemSelected(item)){
-            Log.d("YO","Klik");
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
 
-    @Override
-    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-        int id = item.getItemId();
-        if (id == R.id.examine){
-
-        }
-        else if (id == R.id.create_session){
-
-        }
-        else if (id == R.id.show_session){
-
-
-        }
-        return false;
-    }
 }
--- app/src/main/java/com/example/dev/smartcook/examine.java
+++ app/src/main/java/com/example/dev/smartcook/examine.java
@@ -1,13 +1,26 @@
 package com.example.dev.smartcook;
 
+import android.content.Context;
+import android.content.Intent;
+import android.net.wifi.WifiManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class examine extends AppCompatActivity {
-
+    public Button start_baking;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_examine);
+        start_baking = (Button) findViewById(R.id.start_baking);
+        start_baking.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent baking = new Intent(examine.this, show_session.class);
+                startActivity(baking);
+            }
+        });
     }
 }
